01a61fe88793e57d7c0952f1762b40bbc50d05b5
Andy Tsui <gueei.com@gmail.com>
2012-11-21 11:40:26 +0800

    Converters

--- DocGenerator/ABDocumentation/src/gueei/binding/documentation/ConverterDoclet.java
+++ DocGenerator/ABDocumentation/src/gueei/binding/documentation/ConverterDoclet.java
@@ -3,46 +3,58 @@ package gueei.binding.documentation;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Hashtable;
 
+import com.sun.javadoc.AnnotationDesc;
 import com.sun.javadoc.ClassDoc;
 import com.sun.javadoc.RootDoc;
+import com.sun.javadoc.Tag;
 
 public class ConverterDoclet {
+
+	private static ArrayList<ClassDoc> converters = new ArrayList<ClassDoc>();
 	
 	public static boolean start(RootDoc root){
 		File destDir = new File("output");
 		destDir.mkdirs();
 		
+		
 		ClassDoc[] classes = root.classes();
 		for(int i=0; i<classes.length; i++){
 			processClass(classes[i], destDir);
 		}
+		
+		outputConverters(destDir);
+		
 		return true;
 	}
 	
 	private static void processClass(ClassDoc classDoc, File destDir) {
 		if (classDoc.qualifiedTypeName().startsWith("gueei.binding.converters.")){
-			System.out.println("converter: " + classDoc.qualifiedTypeName());
-			processConverter(classDoc, destDir);
+			// Ignore inner class
+			if (!classDoc.name().contains("."))
+				converters.add(classDoc);
 		}
 		
     }
 
-	private static void processConverter(ClassDoc classDoc, File destDir) {
-		// Ignore inner class
-		if (classDoc.name().contains(".")) return;
-		
-		// Create Converter File
-		String converterName = classDoc.name();
-		File output = new File(destDir.getAbsolutePath(), converterName);
+	private static void outputConverters(File destDir) {
 		try {
+			File output = new File(destDir.getAbsolutePath(), "converters.html");
 	        output.createNewFile();
 			PrintWriter writer = new PrintWriter(output);
 			
 			writer.println("---");
 			writer.println("layout: converter");
-			writer.println("title: " + converterName);
-			writer.println("permalink: converter/" + converterName + ".html");
+			writer.println("title: Converters");
+			writer.println("permalink: converters/index.html");
+			writer.println("converters:");
+
+			for(ClassDoc doc : converters){
+				writeConverter(doc, writer);
+			}
+
 			writer.println("---");
 			
 			writer.close();
@@ -50,5 +62,51 @@ public class ConverterDoclet {
 	        e.printStackTrace();
         } 
 		
+		
     }
+
+	private static void writeConverter(ClassDoc doc, PrintWriter writer) {
+	    String name = doc.name();
+	    writer.println("  - name: " + name);
+	    writer.println("    description: " + doc.commentText().replace("\n", "<br/>"));
+	    Hashtable<String, ArrayList<Tag>> tags = organizeTags(doc.tags());
+
+	    if (tags.containsKey("@return")){
+		    Tag[] returnTags = tags.get("@return").toArray(new Tag[0]);
+		    writer.println("    return: ");
+		    for(int i=0; i<returnTags.length; i++){
+		    	String[] text = returnTags[i].text().split(" ");
+		    	writer.println("        type: " + text[0]);
+		    	writer.println("        type_short: " + ShortTypeName(text[0]));
+		    	writer.println("        comment: " + CommentString(text, 1));
+		    }
+	    }
+    }
+	
+	private static String CommentString(String[] delimited, int start){
+		String output = "";
+		for(int i=start; i<delimited.length; i++){
+			output += delimited[i] + " ";
+		}
+		return output;
+	}
+	
+	private static String ShortTypeName(String TypeName){
+		int lastDot = TypeName.lastIndexOf('.');
+		if (lastDot<0) return TypeName;
+		return TypeName.substring(lastDot+1, TypeName.length());
+	}
+	
+	private static Hashtable<String, ArrayList<Tag>> organizeTags(Tag[] tags){
+		Hashtable<String, ArrayList<Tag>> output = new Hashtable<String, ArrayList<Tag>>();
+		for(int i=0; i<tags.length; i++){
+			Tag tag = tags[i];
+			String name = tag.name();
+			if (!output.containsKey(name)){
+				output.put(name, new ArrayList<Tag>());
+			}
+			output.get(name).add(tag);
+		}
+		return output;
+	}
 }
